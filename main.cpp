6f07506360d95036d2ca5c5a5e900c319be49476
PatrickjHayes <patrickhayes@tipperaryetb.ie>
2024-05-14 16:23:44 +0000

    change

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include "mbed.h"
 
-int n_1 = 0;
-int n_2 = 1;
-int n_3 = 0;
+
 int max_value = 30;
+
+int fib(int n){
+    if(n==0 || n ==0)
+    printf(*);
+    return n;
+    else{
+        printf ("(%d), (%d)" n-1, n-2);
+        return (fib(n-1) + fib(n-2));
+
+    }
+}
 
 // main() runs in its own thread in the OS
 int main()
 {
     printf("lab2 start!!/r/n");
-    printf(series is: %d, %d, ", n_1, n_2")
+    printf(series is: "")
 
-    for(int i=3; i<=max_value; i++){
-        n_3 = n_1 +n_2;
-        printf(%d, ", n_3");
-        n_1 = n_2;
-        n_2 = n_3;
+    for(int i=0; i< max_value; i++){
+        
+        printf(%d, ", fib(i));
+        
 
     }
     printf("/r/n");
